ced212fcbe7ca0cf8ffbac32869c7392c7f16264
veer-pratap-fueldao <veer@fueldao.io>
2024-09-28 12:06:33 +0530

    design updates

--- ssr/src/app.rs
+++ ssr/src/app.rs
@@ -4,6 +4,7 @@ use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
 #[component]
+//test
 pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
